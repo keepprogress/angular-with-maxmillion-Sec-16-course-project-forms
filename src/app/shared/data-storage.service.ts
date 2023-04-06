e8e2b5e52c000308b368bdd8a531a01f08008246
Nick.Hsu <Nick.Hsu@testritegroup.com>
2023-04-06 17:04:38 +0800

    Sec 19. 283. Transforming Response Data.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
+import { map } from "rxjs/operators";
 
 @Injectable({providedIn: 'root' })
 export class DataStorageService {
@@ -18,6 +19,11 @@ export class DataStorageService {
 
   fetchRecipes() {
     this.http.get<Recipe[]>('https://ng-recipe-book-b24bc-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json')
+      .pipe(map(recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+        });
+      }))
       .subscribe(recipes => {
         this.recipeService.setRecipes(recipes);
       })
